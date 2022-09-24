0e9c794bdd89d20495d9ae51651f74defb49e254
gibsonbett <gibson.bett@student.moringaschool.com>
2022-09-24 05:02:38 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    let handleKeyPad = (e) => {
+        return(
+           console.log("Entering password...")// const data_input = 
+        )
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleKeyPad} />
+        </div>
     )
 }
 
